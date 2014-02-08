684204712304da94a4e0d49812e27fbaa61c13b1
Aleksey Leontiev <i@aleontiev.me>
2014-02-08 16:27:50 +0400

    full schedule page

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -2,8 +2,10 @@ class ScheduleController < ApplicationController
   def show
     @schedule = Schedule
       .includes{trip.category}
+      .where{begin_date > DateTime.now}
       .order{trip.id}
       .order{trip.category.id}
+      .order{begin_date}
       .group_by{|x| x.trip.category}
   end
 end
