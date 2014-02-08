0b690cadf4ab0169e2c1409a3176463b6d23f7be
Aleksey Leontiev <i@aleontiev.me>
2014-02-08 15:33:11 +0400

    translation

--- app/admin/trip.rb
+++ app/admin/trip.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Trip do
-  permit_params :name, :description, :category_id, :permalink
+  permit_params :name, :description, :category_id, :permalink, :introduction
 
   def to_param
     permalink
