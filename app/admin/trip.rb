d6e5dfa85339af6a938483bf0779bf84e9a9c91a
Aleksey Leontiev <i@aleontiev.me>
2014-02-15 12:58:09 +0400

    html format. some translations

--- app/admin/trip.rb
+++ app/admin/trip.rb
@@ -1,7 +1,3 @@
 ActiveAdmin.register Trip do
   permit_params :name, :description, :category_id, :permalink, :introduction, :cost
-
-  def to_param
-    permalink
-  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,8 @@
 class Trip < ActiveRecord::Base
   belongs_to :category
   has_many :schedules
+
+  def to_param
+    permalink
+  end
 end
