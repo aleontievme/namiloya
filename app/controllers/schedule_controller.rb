9a4239e827ca82889f7659f1640b357042ff436e
Aleksey Leontiev <i@aleontiev.me>
2014-03-22 13:22:35 +0400

    fix order and price

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -3,10 +3,10 @@ class ScheduleController < ApplicationController
     @schedule = Schedule
       .includes{trip.category}
       .where{begin_date > DateTime.now}
+      .order{begin_date}
       .order{trip.id}
       .order{trip.order_num}
       .order{trip.category.id}
-      .order{begin_date}
       .group_by{|x| x.trip.category}
   end
 end
